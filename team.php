2ddc49d7fbdc9d38beb4417731d656e51c657c42
bigcache-angelu$ <bigcache@hotmail.com>
2017-05-19 02:01:31 -0300

    h5

--- team.php
+++ team.php
@@ -4,7 +4,8 @@
 ?>
     <div id="team-members" class="cf">
         <h1>Nuestro Equipo</h1>
-        <p>Los gigantes de la pizza es una casa de comidas familiar con mas de 20 años en Almagro y estamos orgullosos de eso! <br> <Strong> Nos enorgullecemos de nuestra insuperable pizza a la piedra</Strong></p>
+        <h5>Los gigantes de la pizza es una casa de comidas familiar con mas de 20 años en Almagro y estamos orgullosos de eso!</h5>
+        <p><Strong> No dejes de probar nuestra insuperable pizza a la piedra</Strong></p>
 
         <hr>
         <?php
