861ee02f4f6189342f5b70b6f94d613ee21b3fcb
bigcache-angelu$ <bigcache@hotmail.com>
2017-05-18 22:58:35 -0300

    fotos

--- team.php
+++ team.php
@@ -5,24 +5,24 @@
     <div id="team-members" class="cf">
         <h1>Nuestro Equipo</h1>
         <p>Los gigantes de la pizza es una casa de comidas familiar con mas de 20 años en Almagro y estamos orgullosos de eso! <br> <Strong> Nos enorgullecemos de nuestra insuperable pizza a la piedra</Strong></p>
-    
+
         <hr>
         <?php
         foreach ($teamMembers as $member) {
         ?>
             <div class="member">
-                <img src="IMG/<?php echo $member['name']; ?>.jpg" alt="<?php echo $member['name'];?>">
+                <img src="/IMG/<?php echo $member['name']; ?>.jpg" alt="<?php echo $member['name'];?>">
                 <h2><?php echo $member['name']; ?></h2>
                 <p><?php echo $member['bio']; ?></p>
-                
+
             </div> <!--member-->
         <?php
         }
         ?>
-        
+
     </div><!-- team-members-->
     <hr>
-    
+
 <?php
     include('includes/footer.php');
 ?>
